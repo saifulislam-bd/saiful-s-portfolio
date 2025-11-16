3e7af9679867d9270514fe833784705d751414d4
saifulislam-bd <saifulislam.dev22@gmail.com>
2025-11-16 22:49:01 +0600

    homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
-export default function HomePage() {
-  return <div>Homepage</div>;
-}
+import ContactSection from "@/components/ContactSection";
+import HereSection from "@/components/here-section";
+import ProjectsSection from "@/components/project-section";
+import TechStack from "@/components/TechStack";
+import WorkExperience from "@/components/WorkExperience";
+
+const Home = () => {
+  return (
+    <>
+      <main className="max-w-7xl mx-auto px-6 lg:px-8">
+        <HereSection />
+        <WorkExperience />
+        <TechStack />
+        <ProjectsSection />
+        <ContactSection />
+      </main>
+    </>
+  );
+};
+
+export default Home;
