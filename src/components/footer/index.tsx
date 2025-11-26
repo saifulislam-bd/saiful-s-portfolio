9ee207e4167660a49a7094d34f05ae6fd9ee749b
saifulislam-bd <saifulislam.dev22@gmail.com>
2025-11-26 20:21:35 +0600

    added prettier

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
-    <footer className="mt-24 border-t border-white/10">
-      <div className="max-w-7xl mx-auto px-6 lg:px-8 py-8 text-center">
-        <p className="text-sm text-white/40">
+    <footer className='mt-24 border-t border-white/10'>
+      <div className='max-w-7xl mx-auto px-6 lg:px-8 py-8 text-center'>
+        <p className='text-sm text-white/40'>
           © {new Date().getFullYear()} Saiful Islam. All rights reserved.
         </p>
       </div>
